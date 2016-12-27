92fdd9fd536a862f6a2d868fb4b0de33deb7af8e
buzzlightyear182 <janeheatherdee@gmail.com>
2016-12-27 13:51:58 +0800

    AJAX for searching items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,10 @@ class ItemsController < ApplicationController
   def index
     @q = Item.ransack(params[:q])
     @items = @q.result.paginate(page: params[:page], per_page: 10)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def download
